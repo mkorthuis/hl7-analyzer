a1da4f3d7ef2c69e7e217041359b3e2f29644533
Yoonjin Lee <yoon@candescenthealth.com>
2016-06-30 16:07:48 -0700

    looping through dir and parse the HL7

--- app/scripts/components/home/home.js
+++ app/scripts/components/home/home.js
@@ -3,5 +3,22 @@
 
 angular.module('candescent.analyzer').controller('HomeController',
     function(HL7Parse) {
+        var vm = this;
+        var fs = require('fs');
+        var dir = 'app/hl7/orm';
+        var filesInDir = fs.readdirSync(dir);
 
+        vm.map = {};
+        vm.results = [];
+
+        for(var id in filesInDir){
+            var filename = filesInDir[id];
+            if(filename != '.DS_Store'){
+                var data = fs.readFileSync('app/hl7/orm' + '/' + filename).toString();
+                //console.log(data);
+                vm.results.push(HL7Parse.parseMessage(data));
+
+            }
+        }
+        console.log(vm.results);
 });
