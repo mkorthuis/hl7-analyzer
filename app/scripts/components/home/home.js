bc0c3523bac249a8741e07c60309903bd075b8ed
Yoonjin Lee <yoon@candescenthealth.com>
2016-06-30 17:35:53 -0700

    Analyzing ...

--- app/scripts/components/home/home.js
+++ app/scripts/components/home/home.js
@@ -1,24 +1,62 @@
-
 'use strict';
 
 angular.module('candescent.analyzer').controller('HomeController',
-    function(HL7Parse) {
-        var vm = this;
-        vm.dir = 'app/hl7/orm';
-        var fs = require('fs');
-        var filesInDir = fs.readdirSync(vm.dir);
+   function (HL7Parse) {
+      var vm = this;
+      var fs = require('fs');
+      var dir = 'app/hl7/orm';
+      var filesInDir = fs.readdirSync(dir);
 
-        vm.map = {};
-        vm.results = [];
+      vm.map = {};
+      vm.results = [];
+      vm.stats = [];
 
-        for(var id in filesInDir){
-            var filename = filesInDir[id];
-            if(filename != '.DS_Store'){
-                var data = fs.readFileSync('app/hl7/orm' + '/' + filename).toString();
-                //console.log(data);
-                vm.results.push(HL7Parse.parseMessage(data));
+      for (var id in filesInDir) {
+         var filename = filesInDir[id];
+         if (filename != '.DS_Store') {
+            var data = fs.readFileSync(dir + '/' + filename).toString();
+            //console.log(dir + '/' + filename);
+            console.log(HL7Parse.parseMessage(data));
+            vm.results.push(HL7Parse.parseMessage(data));
+         }
+      }
 
+      for (var id in vm.results) {
+         var hl7 = vm.results[id];
+         for (var segmentId in hl7) {
+            console.log(segmentId);
+            var segment = hl7[segmentId];
+            if (segmentId == 'MSH') {
+               for (var field in segment) {
+                  //console.log(field + ':' + segment[field]);
+                  var stats = {
+                     'length': (segment[field]).length,
+                     'description': field,
+                     'value': segment[field]
+                  }
+                  vm.stats.push(stats);
+               }
+            } else {
+               for (var index in segment) {
+                  var data = segment[index];
+                  for (var field in data) {
+                     //console.log(field + ':' + data[field]);
+                     var stats = {
+                        'length': (data[field]).length,
+                        'description': field,
+                        'value': data[field]
+                     }
+                     vm.stats.push(stats);
+                  }
+               }
             }
-        }
-        console.log(vm.results);
-});
+         }
+      }
+
+
+      console.log(vm.stats);
+
+
+      //console.log(vm.results);
+
+   });
